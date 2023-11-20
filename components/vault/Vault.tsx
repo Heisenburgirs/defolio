7dfc3a87263c4486e42375195313a2a5fe210bb5
heisenburgers <zkdaoorganization@gmail.com>
2023-11-20 18:30:07 +0400

    vault deployment

--- components/vault/Vault.tsx
+++ components/vault/Vault.tsx
@@ -4,20 +4,50 @@ import LSP9Vault from '@lukso/lsp-smart-contracts/artifacts/LSP9Vault.json';
 import UniversalProfile from '@lukso/lsp-smart-contracts/artifacts/UniversalProfile.json';
 import ERC725, { ERC725JSONSchema } from '@erc725/erc725.js';
 import { VaultSchema } from '../schema/DeployedVaults'
+import { VaultName } from '../schema/VaultName'
+import { VaultDescription } from '../schema/VaultDescription'
+import { useEffect, useState } from "react";
+import Image from 'next/image'
+import lightPurpleArrow from '@/public/icons/lightPurple_arrow.png';
+import purpleArrow from '@/public/icons/purple_arrow.png';
 
 const Vault = () => {
-  const { address } = useAccount()
+  const { address, isConnected } = useAccount()
 
-  const test = async () => {
-    const provider = new ethers.providers.Web3Provider(window.lukso);
+  const [addVault, setAddVault] = useState(false)
+  const [hover, setHover] = useState(false);
+
+  const [hasProvidedName, setHasProvidedName] = useState(false);
+  const [hasProvidedDesc, setHasProvidedDesc] = useState(false);
+  const [name, setName] = useState('')
+  const [desc, setDesc] = useState('')
+
+  useEffect(() => {
+    setHasProvidedName(!!name);
+    setHasProvidedDesc(!!desc);
+  }, [name, desc]);
+
+  const getData = async () => {
     const myErc725Contract = new ERC725(VaultSchema, address, "https://rpc.testnet.lukso.network");
 
     const fetch = await myErc725Contract.fetchData(`VaultsDeployed[]`)
 
     console.log("DATA FETCHED", fetch)
+
+    const myErc725Contract2 = new ERC725(VaultName, address, "https://rpc.testnet.lukso.network");
+
+    const fetch2 = await myErc725Contract2.fetchData({keyName: "VaultName:<address>", dynamicKeyParts: "0x4899eC2046B60C4Dd29eB5B148A97AA47d93e210"})
+
+    console.log("DATA FETCHED", fetch2)
+
+    const myErc725Contract3 = new ERC725(VaultDescription, address, "https://rpc.testnet.lukso.network");
+
+    const fetch3 = await myErc725Contract3.fetchData({keyName: "VaultDescription:<address>", dynamicKeyParts: "0x4899eC2046B60C4Dd29eB5B148A97AA47d93e210"})
+
+    console.log("DATA FETCHED", fetch3)
   }
 
-  const notify = async () => {
+  const deployVault = async () => {
     const provider = new ethers.providers.Web3Provider(window.lukso);
     const signer = provider.getSigner();
     const myUniversalProfile = new ethers.Contract(address || '', UniversalProfile.abi, signer);
@@ -33,41 +63,151 @@ const Vault = () => {
       'https://rpc.testnet.lukso.network'
     );
 
+    const erc7255 = new ERC725(
+      VaultName,
+      address,
+      'https://rpc.testnet.lukso.network'
+    );
+
+    const erc72555 = new ERC725(
+      VaultDescription,
+      address,
+      'https://rpc.testnet.lukso.network'
+    );
+
     try {
 
-      console.log("setdata");
+      const deployVault = await vaultFactory.connect(signer).deploy(address);
 
-      const array = ['0xfB8a1f71669B1171dbc50fac869f02223EcfEA8F', '0x368731AE2E23e72751C432A935A2CF686Bb72AAC']
+      console.log("Vault deployed: ", deployVault.address)
 
-      const data = erc725.encodeData([
-        {
-          keyName: 'VaultsDeployed[]',
-          value: array,
-        }
-      ]);
+      try {
+        const targetVaultAddress = deployVault.address;
 
-      const setData = await myUniversalProfile.setDataBatch(data.keys, data.values);
+        const data = erc725.encodeData([
+          {
+            keyName: 'VaultsDeployed[]',
+            value: [targetVaultAddress],
+          }
+        ]);
 
-      const receipt = await setData.wait()
+        const data2 = erc7255.encodeData([
+          {
+            keyName: "VaultName:<address>",
+            dynamicKeyParts: targetVaultAddress,
+            value: name,
+          }
+        ]);
 
-      console.log("RESULTS ARE IN", receipt);
+        const data3 = erc72555.encodeData([
+          {
+            keyName: "VaultDescription:<address>",
+            dynamicKeyParts: targetVaultAddress,
+            value: desc,
+          }
+        ]);
 
-      /*const deployVault = await vaultFactory.connect(signer).deploy(address);
+        // Assuming data, data2, and data3 are correctly encoded
+        const allKeys = [...data.keys, ...data2.keys, ...data3.keys];
+        const allValues = [...data.values, ...data2.values, ...data3.values];
+
+
+        const setData = await myUniversalProfile.setDataBatch(allKeys, allValues);
+
+        const txResult = await setData.wait()
+
+        console.log("RESULTS ARE IN", txResult);
+
+      } catch (err) {
+        console.log("ERROR SETTING DATA")
+      }
+
       
-      const receipt = await deployVault.wait();
-
-      console.log("Vault deployed: ", receipt)*/
     } catch (error) {
-  
-      console.log("ERRPR", error)
+      console.log("ERROR DEPLOYING VAULT", error)
     }
-
   }
+
+  const test = async() => {
+    console.log(name)
+    console.log(desc)
+  }
+
   return (
     <div className="flex w-full h-full bg-white shadow rounded-15">
-      <div>
-        <div className="flex bg-black rounded-15 py-2 px-4 hover:cursor-pointer text-white" onClick={notify}>Test</div>
-        <div className="flex bg-black rounded-15 py-2 px-4 hover:cursor-pointer text-white" onClick={test}>EGAgasga</div>
+      <div className="flex flex-col py-8 px-6 w-full h-full gap-12">
+        {addVault ? (
+          <>
+            <title>New Vault</title>
+            <div
+              className="flex gap-2 px-4 items-center text-lightPurple hover:text-purple hover:cursor-pointer transition text-small"
+              onMouseEnter={() => setHover(true)}
+              onMouseLeave={() => setHover(false)}
+              onClick={() => {setAddVault(false)}}
+            >
+              <div 
+                className="transition ease-in-out duration-200"
+              >
+                <Image 
+                  src={hover ? purpleArrow : lightPurpleArrow} 
+                  width={24} 
+                  height={24} 
+                  alt="Back"
+                  className="hover:cursor-pointer"
+                />
+              </div>
+              <div>Back</div>
+            </div>
+            <div className="flex flex-col w-full h-full items-center py-8 gap-16">
+              <h1 className="text-medium font-bold text-purple">Deploy New Vault</h1>
+              <div className="flex flex-col gap-6 w-[400px]">
+                <div className="flex flex-col gap-2">
+                  <h2 className="text-lightPurple">Name</h2>
+                  <input type="text" className="rounded-15 border border-lightPurple focus:outline-purple py-2 px-6 text-purple font-bold" placeholder="Enter vault name..."
+                  onChange={(e) => { setName(e.target.value)}}
+                  />
+                </div>
+                <div className="flex flex-col gap-2">
+                  <h2 className="text-lightPurple">Description</h2>
+                  <textarea className="h-[350px] rounded-15 border border-lightPurple focus:outline-purple py-2 px-6 text-purple font-bold" placeholder="Enter vault description..."
+                    onChange={(e) => { setDesc(e.target.value)}}
+                  />
+                </div>
+              </div>
+              <button 
+                className={
+                  `py-2 px-6 bg-lightPurple bg-opacity-75 text-medium rounded-15 text-white transition
+                  ${hasProvidedName && hasProvidedDesc ? 'bg-purple bg-opacity-100' : 'cursor-not-allowed opacity-50'}`
+                }
+                onClick={deployVault}
+                disabled={!hasProvidedName || !hasProvidedDesc}
+              >
+                Finalize Vault
+              </button>
+              <div onClick={getData}>test</div>
+            </div>
+          </>
+        )
+        :
+        (
+          <>
+            <div className="flex w-full justify-between">
+              <h1 className="text-medium text-purple font-bold">Your Vaults</h1>
+              <div onClick={() => { if (isConnected) setAddVault(true) }} className="w-[135px] py-2 px-4 rounded-15 text-xsmall border border-lightPurple text-purple font-bold hover:cursor-pointer hover:bg-purple hover:text-white transition">
+                Create a Vault
+              </div>
+            </div>
+            <div className="flex w-full h-full">
+              <div className="w-[250px] h-[250px] border border-lightPurple rounded-15 py-6 px-4 flex flex-col justify-between">
+                <div className="flex flex-col gap-2">
+                  <div className="text-medium text-purple font-bold">Vault Name</div>
+                  <div className="text-purple text-xsmall font-bold opacity-75">0x012...15fd</div>
+                </div>
+                <div className="w-full text-center py-2 rounded-15 border border-lightPurple text-purple hover:cursor-pointer hover:bg-lightPurple hover:text-white transition">Manage</div>
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
